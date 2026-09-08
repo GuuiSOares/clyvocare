6a720719add9cbe3524336dd2ecb55af868f0e61
GuuiSOares <guilherme.soaresdealmeida.158@gmail.com>
2026-09-08 13:07:25 -0300

    Corrigido a permissão do Key Vault para gravar os segredos com a conta da FIAP.

--- scripts/03_key-vault.sh
+++ scripts/03_key-vault.sh
@@ -11,19 +11,29 @@ acrName="clyvocare$rm"
 ACRUSERNAME=$(az acr credential show --name "$acrName" --resource-group "$resourceGroup" --query username --output tsv)
 ACRPASSWORD=$(az acr credential show --name "$acrName" --resource-group "$resourceGroup" --query passwords[0].value --output tsv)
 keyVaultName="kv-clyvo-$rm"
+objectId=$(az ad signed-in-user show --query id -o tsv)
 
 az provider register --namespace Microsoft.KeyVault
 
 if ! az keyvault show --name "$keyVaultName" --resource-group "$resourceGroup" &> /dev/null; then
-  az keyvault create --name "$keyVaultName" --resource-group "$resourceGroup" --location "$location" --enable-rbac-authorization true
+  az keyvault create \
+    --name "$keyVaultName" \
+    --resource-group "$resourceGroup" \
+    --location "$location" \
+    --enable-rbac-authorization false
+else
+  az keyvault update \
+    --name "$keyVaultName" \
+    --resource-group "$resourceGroup" \
+    --enable-rbac-authorization false
 fi
 
-az role assignment create \
-  --assignee $(az account show --query user.name -o tsv) \
-  --role "Key Vault Administrator" \
-  --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/$resourceGroup/providers/Microsoft.KeyVault/vaults/$keyVaultName
+az keyvault set-policy \
+  --name "$keyVaultName" \
+  --object-id "$objectId" \
+  --secret-permissions get list set delete
 
-sleep 15
+sleep 20
 
 az keyvault secret set --vault-name "$keyVaultName" --name oracle-password --value "$ORACLE_PASSWORD"
 az keyvault secret set --vault-name "$keyVaultName" --name oracle-app-user --value "$APP_USER"
